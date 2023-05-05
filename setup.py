c968eb76eba472b866f4ec3e458948a5a23004de
dibyendubiswas1998 <dibyendubiswas1998@gmail.com>
2023-05-05 21:58:44 +0530

    add setup.py file

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="src",
+    version="v.0.0.1",
+    author="dibyendubiswas1998",
+    author_email="dibyendubiswas1998@gmail.com",
+    description="It's a complete Pipeline for this project",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dibyendubiswas1998/Credit-Card-Default-Prediction.git",
+    packages=["src"],
+    license="GNU",
+    python_requires=">=3.10",
+    install_requires=[
+        'dvc',
+        "dvc[gdrive]",
+        'pandas',
+        'scikit-learn'
+    ]
+)
